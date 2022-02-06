02808648754080fb1890dce9a204708baed0dcd5
STUDIO-PAGURO <studiopaguro@gmail.com>
2022-02-06 20:05:10 +0900

    新規登録かログインしないとタイムラインにいけない仕様に変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:edit, :show]
-  before_action :authenticate_user!, except: [:index, :show]
+  # before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @posts = Post.includes(:user).order("created_at DESC")
