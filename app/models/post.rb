86144975dc988fcf94cd839a93d3777ca4045433
STUDIO-PAGURO <studiopaguro@gmail.com>
2022-02-10 19:14:12 +0900

    基本的いいね機能実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,14 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_one_attached :image
+  has_many :likes, dependent: :destroy
+  has_many :liked_users, through: :likes, source: :user
 
   def was_attached?
     self.image.attached?
+  end
+
+  def liked_by?(user)
+    likes.where(user_id: user.id).exists?
   end
 end
