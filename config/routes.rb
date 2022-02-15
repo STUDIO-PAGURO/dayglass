c90cdf845dc3511f3a79c5f76d67c3abdafac0b1
STUDIO-PAGURO <studiopaguro@gmail.com>
2022-02-15 17:20:56 +0900

    フォロー機能実装完了

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,9 @@ Rails.application.routes.draw do
     end
   end
   resources :users, only: [:show, :edit, :update]
+  
+  post 'follow/:id', to: 'relationships#follow', as: 'follow'
+  post 'unfollow/:id', to: 'relationships#unfollow', as: 'unfollow'
+  get 'users/following/:user_id', to: 'users#following', as: 'users_following'
+  get 'users/follower/:user_id', to: 'users#follower', as: 'users_follower'
 end
