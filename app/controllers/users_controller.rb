7610f85f1b119d9e77b15d7965153b23da882246
STUDIO-PAGURO <studiopaguro@gmail.com>
2022-02-15 17:20:56 +0900

    フォロー機能実装完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,9 @@
 class UsersController < ApplicationController
   def show
-    user = User.find(params[:id])
-    @id = user.id
-    @name = user.name
-    @account = user.account
-    @posts = user.posts.order("created_at DESC")
-    @profile = user.profile
-    @icon = user.icon
+    @user = User.find(params[:id])
+    @posts = @user.posts.order("created_at DESC")
+    @followings = @user.following_user
+    @followers = @user.follower_user
   end
 
   def edit
@@ -20,6 +17,18 @@ class UsersController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def following
+    @user = User.find(params[:user_id])
+    @followings = @user.following_user
+    @followers = @user.follower_user
+  end
+
+  def follower
+    @user = User.find(params[:user_id])
+    @followers = @user.follower_user
+    @followings = @user.following_user
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,26 @@ class User < ApplicationRecord
   has_one_attached :icon
   has_many :likes
 
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :following_user, through: :follower, source: :followed
+  has_many :follower_user, through: :followed, source: :follower
+
   validates :name, :account, presence: true
   validates :password, presence: true, on: :create
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: "には英字と数字の両方を含めて設定してください",  on: :create
+
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    following_user.include?(user)
+  end
 end
