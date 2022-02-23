bc7b4185e04848b1c955bb8d5c22fa5b0c6816ad
STUDIO-PAGURO <studiopaguro@gmail.com>
2022-02-23 18:00:42 +0900

    repostキャンセル

--- db/migrate/20220223040544_create_reposts.rb
+++
@@ -1,9 +0,0 @@
-class CreateReposts < ActiveRecord::Migration[6.0]
-  def change
-    create_table :reposts do |t|
-      t.references :user, foreign_key: true
-      t.references :post, foreign_key: true
-      t.timestamps
-    end
-  end
-end
--- spec/factories/reposts.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :repost do
-    
-  end
-end
