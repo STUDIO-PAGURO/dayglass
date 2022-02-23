2371df41aa19e367a51e909715df9535fb495075
STUDIO-PAGURO <studiopaguro@gmail.com>
2022-02-23 18:00:42 +0900

    repostキャンセル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_23_040544) do
+ActiveRecord::Schema.define(version: 2022_02_15_050616) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
