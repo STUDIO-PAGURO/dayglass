50edc59072507967b6d12ab63f1173a53618a819
STUDIO-PAGURO <studiopaguro@gmail.com>
2022-03-04 17:38:02 +0900

    accountのヴァリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   has_many :posts, dependent: :destroy
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_one_attached :icon
-  has_many :likes
+  has_many :likes, dependent: :destroy
 
   has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
@@ -18,6 +18,8 @@ class User < ApplicationRecord
   validates :password, presence: true, on: :create
   validates :account, uniqueness: true
 
+  VALID_ACCOUNT_REGEX = /\A[\w\d.@_-]+\z/i
+  validates :account, format: { with: VALID_ACCOUNT_REGEX }
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: "には英字と数字の両方を含めて設定してください",  on: [:create, :update]
 
