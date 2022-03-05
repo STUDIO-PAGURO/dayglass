3ea4e26838260df06a343ca1aaf5767a9773eb49
STUDIO-PAGURO <studiopaguro@gmail.com>
2022-03-05 15:08:56 +0900

    postモデルテスト

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#create" do
+    before do
+      @post = FactoryBot.build(:post)
+    end
+
+    it "textかimageが存在していれば保存できる" do
+      expect(@post).to be_valid
+    end
+
+    it "textが空でも保存できる" do
+      @post.text = ""
+      expect(@post).to be_valid
+    end
+
+    it "imageが空でも保存できる" do
+      @post.image = nil
+      expect(@post).to be_valid
+    end
+
+    it "textとimageが空では保存できない" do
+      @post.text = ""
+      @post.image = nil
+      @post.valid?
+      expect(@post.errors.full_messages).to include("テキストを入力してください")
+    end
+  end
 end
