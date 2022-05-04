96f70011f123d625409cc50a79054568ec2b582a
STUDIO-PAGURO <studiopaguro@gmail.com>
2022-05-04 16:46:33 +0900

    Postsコントローラーテスト

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :request do
+
+  before do
+    @post = FactoryBot.create(:post)
+  end
+
   describe "GET #index" do
     context "未ログインの場合ログインページにリダイレクトされる" do
       it "302が返ってくる" do
@@ -14,9 +19,8 @@ RSpec.describe PostsController, type: :request do
       end
     end
 
-    context "indexアクションにリクエストする" do
+    context "ログインしてindexアクションにリクエストする" do
       before do
-        @post = FactoryBot.create(:post)
         sign_in @post.user
       end
       
@@ -28,6 +32,51 @@ RSpec.describe PostsController, type: :request do
       it "indexアクションにリクエストするとレスポンスに投稿済みのテキストが存在する" do
         get root_path
         expect(response.body).to include(@post.text)
+      end
+
+      it "indexアクションにリクエストするとレスポンスに24時間以内に投稿済みのテキストが存在する" do
+        get root_path
+        expect(response.body).to include(@post.text)
+      end
+    end
+  end
+
+  describe "GET #show" do
+    context "未ログインの場合ログインページにリダイレクトされる" do
+      it "302が返ってくる" do
+        get post_path(@post)
+        expect(response.status).to eq 302
+      end
+
+      it "indexアクションを行うとサインインページに遷移する" do
+        get post_path(@post)
+        expect(response).to redirect_to "/users/sign_in"
+      end
+    end
+
+    context "ログインしてshowアクションにリクエストする" do
+      before do
+        sign_in @post.user
+      end
+
+      it "showアクションにリクエストすると正常にレスポンスが返ってくる" do
+        get post_path(@post)
+        expect(response.status).to eq 200
+      end
+
+      it "showアクションにリクエストするとレスポンスに投稿済みのテキストが存在する" do
+        get post_path(@post)
+        expect(response.body).to include(@post.text)
+      end
+
+      it "showアクションにリクエストするとレスポンスに投稿済みの画像が存在する" do
+        get post_path(@post)
+        expect(response.body).to include("post-image")
+      end
+
+      it "showアクションにリクエストするとレスポンスにコメント投稿部分が存在する" do
+        get post_path(@post)
+        expect(response.body).to include("コメントする")
       end
     end
   end
