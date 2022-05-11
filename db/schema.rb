d4121d10901074041b92a672ff2350e07b59f395
STUDIO-PAGURO <studiopaguro@gmail.com>
2022-05-11 17:09:40 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -71,15 +71,6 @@ ActiveRecord::Schema.define(version: 2022_02_15_050616) do
     t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
-  create_table "reposts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "post_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["post_id"], name: "index_reposts_on_post_id"
-    t.index ["user_id"], name: "index_reposts_on_user_id"
-  end
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "account", null: false
@@ -104,6 +95,4 @@ ActiveRecord::Schema.define(version: 2022_02_15_050616) do
   add_foreign_key "posts", "users"
   add_foreign_key "relationships", "users", column: "followed_id"
   add_foreign_key "relationships", "users", column: "follower_id"
-  add_foreign_key "reposts", "posts"
-  add_foreign_key "reposts", "users"
 end
