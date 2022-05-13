8b5717ee195c8eca39ca4484d41d8dc11279d180
STUDIO-PAGURO <studiopaguro@gmail.com>
2022-05-13 15:38:05 +0900

    編集

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,16 +67,16 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
   # config.action_mailer.perform_caching = true
-  config.action_mailer.default_url_options = { protocol: "https", host: "dayglass.herokuapp.com" }
+  config.action_mailer.default_url_options = { protocol: "https", host: "https://dayglass.herokuapp.com/" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :enable_starttls_auto => true,
     :address => "smtp.gmail.com",
-    :port => '587',
-    :domain => 'gmail.com',
+    :port => 587,
+    :domain => 'smtp.gmail.com',
     :user_name => Rails.application.credentials.google[:GOOGLE_MAIL_ADDRESS],
     :password => Rails.application.credentials.google[:GOOGLE_MAILER_PASSWORD],
-    :authentication => :plain
+    :authentication => 'login'
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
