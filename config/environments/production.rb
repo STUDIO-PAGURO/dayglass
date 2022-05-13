c30c6fc70d452fb0375607ca9ce9ef19e745ebe3
STUDIO-PAGURO <studiopaguro@gmail.com>
2022-05-13 11:13:14 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
   # config.action_mailer.perform_caching = true
-  # config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.default_url_options = { protocol: "https", host: "dayglass.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :enable_starttls_auto => true,
